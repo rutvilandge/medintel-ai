5b221a1eb516e1e55e689956bf6f49a9826ba6cf
Rutvi Landge <rutvilandge@gmail.com>
2026-07-14 23:06:00 +0530

    Update page.tsx

--- app/dashboard/prediction/page.tsx
+++ app/dashboard/prediction/page.tsx
@@ -45,7 +45,7 @@ export default async function PredictionPage() {
 
         {/* Prediction Form */}
 
-        <PredictionForm patients={patients} />
+        <PredictionForm />
 
         {/* Statistics */}
 
